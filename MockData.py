3f918b7f2c962504008bb5d2c2bf4e82ad7e9bd4
Ryan Simpson <ryan@bonfr.com>
2017-06-07 20:16:47 -0700

    Update MockData
    The data works now.

--- MockData.py
+++ MockData.py
@@ -1,3 +1,3 @@
-e_data = [("John", "Doe", 1234, "johndoe@gmail.com"), ("Mary", "Jane", 5678), ("Spider", "Man", 1234, "spiderman@gmail.com")]
-acorns_data = {("Johnny", "Doe", 1234): "Johnny's id", ("Liz", "Jane", 5678): "Mary's id",
-          ("Spider", "Man", 1234): "Spiderman's id"}
+e_data = [("John Doe", 1234, "johndoe@gmail.com"), ("Elizabeth Jane", 5678), ("Spiderman", 1234, "spiderman@gmail.com")]
+acorns_data = {("Johnny Doe", 1234): "Johnny's id", ("Liz Jane", 5678): "Liz's id",("Spiderman", 1234): "Spiderman's id"
+    , ("Jonathan Doe", 1234): "Jonathan's id"}
