d6a0218fdced1e7738820245d2287325f3185fbe
Ryan Simpson <ryan@bonfr.com>
2017-06-13 10:32:17 -0700

    Unnecessary

--- MockData.py
+++
@@ -1,3 +0,0 @@
-e_data = [("John Doe", 1234, "johndoe@gmail.com"), ("Elizabeth Jane", 5678), ("Spiderman", 1234, "spiderman@gmail.com")]
-acorns_data = {("Johnny Doe", 1234): "Johnny's id", ("Liz Jane", 5678): "Liz's id",("Spiderman", 1234): "Spiderman's id"
-    , ("Jonathan Doe", 1234): "Jonathan's id"}
